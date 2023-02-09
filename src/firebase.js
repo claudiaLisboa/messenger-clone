ebd52bfcf47cd5e972f9842c5d86b5474e110624
Claudia Lisboa <ccalumby@gmail.com>
2023-02-09 16:44:03 -0500

    add firebase authentification google and facebook

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+
+export const auth = firebase.initializeApp ({
+    apiKey: "AIzaSyCxdEnBGHRfC1wTk34w0taSYNuS9gXKamY",
+    authDomain: "messenger-clone-3e824.firebaseapp.com",
+    projectId: "messenger-clone-3e824",
+    storageBucket: "messenger-clone-3e824.appspot.com",
+    messagingSenderId: "481980457665",
+    appId: "1:481980457665:web:372edf0752deab1a10ea40"
+}).auth();
+
