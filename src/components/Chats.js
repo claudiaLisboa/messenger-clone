bf8c989d8dcf1b47eee83645b2bbcafa6b5f24d3
Claudia Lisboa <ccalumby@gmail.com>
2023-02-12 13:49:30 -0500

    updated chats components and fixed somme bugs

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -6,51 +6,81 @@ import {useAuth} from "../contexts/AuthContext";
 import axios from "axios";
 
 
-const Chats = () => {
-    const navigate = useNavigate();
-    const { user } = useAuth();
-    const [loading, setLoading]  = useState(true);
-    const handleLogout = async () => {
-        await auth.signOut();
-
-        navigate('/');
-    }
-
-    useEffect(() =>{
-        if(!user){
+export default  function Chats () {
+       const didMountRef = useRef(false)
+        const navigate = useNavigate();
+        const { user } = useAuth();
+        const [loading, setLoading]  = useState(true);
+    async function handleLogout() {
+            await auth.signOut()
             navigate('/');
-
-            return;
+    }
+    // add user img
+     async function getFile(url) {
+         let response = await fetch(url);
+         let data = await response.blob();
+             return new File([data],"userPhoto.jpg", {type: 'image/jpeg'});
         }
 
-        axios.get('https://api.chatengine.io/users/me', {
-            headers:{
-                "project-id": "2b492a02-0660-454d-887b-4827497de028",
-                "user-id": user.email,
-                "user-secret": user.uid,
-            }
-        }).then(() =>{
-                setLoading(false);
-        })
+        useEffect(() =>{
+            if (!didMountRef.current) {
+                  didMountRef.current = true
 
-    }, [user,navigate]);
+                if (!user || user === null) {
+                       navigate('/')
+                        return
+            }
+
+        axios.get('https://api.chatengine.io/users/me/ ',
+            { headers: {
+                    "project-id": '2b492a02-0660-454d-887b-4827497de028',
+                    "user-id": user.email,
+                    "user-secret": user.uid
+                }}
+        )
+            .then(() =>  setLoading(false))
+
+            .catch((error) =>{
+                    let formdata = new FormData();
+                        formdata.append('email', user.email)
+                        formdata.append('username',  user.email)
+                        formdata.append('secret', user.uid)
+
+            getFile(user.photoURL)
+                .then(avatar => {
+                    formdata.append('avatar', avatar, avatar.name)
+
+                    axios.post('https://api.chatengine.io/users/',
+                            formdata,
+                            { headers:  { "private-key":  process.env.REACT_APP_CHAT_ENGINE_KEY }}
+                                    //{ "private-key": "21231bc4-a049-4d5d-bb82-3ebf1ae34088" }}
+                     )
+                        .then(() => setLoading(false))
+                        .catch(error => console.log('error'. error.response))
+                    })
+                })
+         }
+    }, [user,navigate])
+
+
+    if(!user || loading ) return <div />
+
     return(
         <div className="chats-page">
             <div className="nav-bar">
-                MessengerClone
-            </div>
-            <div onClick={handleLogout} className="logout-tab">
-                Logout
+                <div className='logo-tab'>
+                      MessengerClone
+               </div>
+                <div onClick={handleLogout} className="logout-tab">
+                     Logout
+                </div>
             </div>
             <ChatEngine
-                height="calc(100vh - 66px"
-                projectId= "2b492a02-0660-454d-887b-4827497de028"
-                userName="."
-                userSecret="."
-
+                height='calc(100vh - 66px)'
+                projectID= '2b492a02-0660-454d-887b-4827497de028'
+                userName={user.email}
+                userSecret={user.uid}
             />
         </div>
-    );
-
+    )
 }
-export default Chats;
--- src/firebase.js
+++ src/firebase.js
@@ -3,11 +3,22 @@ import "firebase/compat/auth";
 
 
 export const auth = firebase.initializeApp ({
-    apiKey: "AIzaSyCxdEnBGHRfC1wTk34w0taSYNuS9gXKamY",
-    authDomain: "messenger-clone-3e824.firebaseapp.com",
-    projectId: "messenger-clone-3e824",
-    storageBucket: "messenger-clone-3e824.appspot.com",
-    messagingSenderId: "481980457665",
-    appId: "1:481980457665:web:372edf0752deab1a10ea40"
+            apiKey: "AIzaSyCxdEnBGHRfC1wTk34w0taSYNuS9gXKamY",
+            authDomain: "messenger-clone-3e824.firebaseapp.com",
+            projectId: "messenger-clone-3e824",
+            storageBucket: "messenger-clone-3e824.appspot.com",
+            messagingSenderId: "481980457665",
+            appId: "1:481980457665:web:372edf0752deab1a10ea40"
 }).auth();
 
+
+    // export const firebaseApp = firebase.initializeApp ({
+    // apiKey: "AIzaSyCxdEnBGHRfC1wTk34w0taSYNuS9gXKamY",
+    // authDomain: "messenger-clone-3e824.firebaseapp.com",
+    // projectId: "messenger-clone-3e824",
+    // storageBucket: "messenger-clone-3e824.appspot.com",
+    // messagingSenderId: "481980457665",
+    // appId: "1:481980457665:web:372edf0752deab1a10ea40"
+    // })
+    // const db = firebaseApp.firestore();
+    // export default db;
