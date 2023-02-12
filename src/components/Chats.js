672b974684be5c3384e7392ad5604fac865d7296
Claudia Lisboa <ccalumby@gmail.com>
2023-02-11 20:39:44 -0500

    created user and added axios

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ChatEngine } from 'react-chat-engine';
 import { auth } from '../firebase';
+import {useAuth} from "../contexts/AuthContext";
+import axios from "axios";
+
 
 const Chats = () => {
     const navigate = useNavigate();
+    const { user } = useAuth();
+    const [loading, setLoading]  = useState(true);
     const handleLogout = async () => {
         await auth.signOut();
 
         navigate('/');
     }
 
+    useEffect(() =>{
+        if(!user){
+            navigate('/');
+
+            return;
+        }
+
+        axios.get('https://api.chatengine.io/users/me', {
+            headers:{
+                "project-id": "2b492a02-0660-454d-887b-4827497de028",
+                "user-id": user.email,
+                "user-secret": user.uid,
+            }
+        }).then(() =>{
+                setLoading(false);
+        })
+
+    }, [user,navigate]);
     return(
         <div className="chats-page">
             <div className="nav-bar">
