0df9eabec00f3079abb6db0ad5c88fcd94cbdb35
Claudia Lisboa <ccalumby@gmail.com>
2023-02-15 15:55:19 -0500

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+
+
+export const auth = firebase.initializeApp ({
+            apiKey: process.env.REACT_APP_API_FIREBASE_KEY,
+            authDomain: "messenger-clone-3e824.firebaseapp.com",
+            projectId: "messenger-clone-3e824",
+            storageBucket: "messenger-clone-3e824.appspot.com",
+            messagingSenderId: process.env.REACT_APP_API_FIREBASE_MESSAGING_SENDER_I,
+            appId: process.env.REACT_APP_API_FIREBASE_APP_ID
+}).auth();
+
