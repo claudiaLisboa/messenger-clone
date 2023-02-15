b799504025fd9b02597412252ce9296d2505c3af
Claudia Lisboa <ccalumby@gmail.com>
2023-02-15 15:53:10 -0500

    created variable for storage secretes keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,23 +2,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 
+
 export const auth = firebase.initializeApp ({
-            apiKey: "AIzaSyCxdEnBGHRfC1wTk34w0taSYNuS9gXKamY",
+            apiKey: process.env.REACT_APP_API_FIREBASE_KEY,
             authDomain: "messenger-clone-3e824.firebaseapp.com",
             projectId: "messenger-clone-3e824",
             storageBucket: "messenger-clone-3e824.appspot.com",
-            messagingSenderId: "481980457665",
-            appId: "1:481980457665:web:372edf0752deab1a10ea40"
+            messagingSenderId: process.env.REACT_APP_API_FIREBASE_MESSAGING_SENDER_I,
+            appId: process.env.REACT_APP_API_FIREBASE_APP_ID
 }).auth();
 
-
-    // export const firebaseApp = firebase.initializeApp ({
-    // apiKey: "AIzaSyCxdEnBGHRfC1wTk34w0taSYNuS9gXKamY",
-    // authDomain: "messenger-clone-3e824.firebaseapp.com",
-    // projectId: "messenger-clone-3e824",
-    // storageBucket: "messenger-clone-3e824.appspot.com",
-    // messagingSenderId: "481980457665",
-    // appId: "1:481980457665:web:372edf0752deab1a10ea40"
-    // })
-    // const db = firebaseApp.firestore();
-    // export default db;
