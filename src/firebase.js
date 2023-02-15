2afa6ba6dfabba3b1a99a2e90107d038c08d89be
Claudia Lisboa <ccalumby@gmail.com>
2023-02-15 16:05:58 -0500

    added new fille firebase because Key was exposed

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 
-
+/*added new fille firebase because Key was exposed*/
 export const auth = firebase.initializeApp ({
             apiKey: process.env.REACT_APP_API_FIREBASE_KEY,
             authDomain: "messenger-clone-3e824.firebaseapp.com",
